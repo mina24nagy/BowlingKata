78a56de1556c994b23d4570901a0e16d38b25b32
Mina Nagy <mina.nagy@jrm.com.eg>
2025-12-25 01:09:11 +0200

    initial commit, creating the kata and handling the basic zero roll and open frame one

---
+++ BowlingGame/BowlingGame.cs
@@ -0,0 +1,16 @@
+﻿namespace BowlingGame
+{
+    public class BowlingGame
+	{
+        int score = 0;
+        public void Roll(int pins)
+        {
+            score += pins;
+        }
+
+        public int Score()
+        {
+            return score;
+        }
+    }
+}
---
+++ BowlingKata.Tests/BowlingGameTests.cs
@@ -0,0 +1,48 @@
+﻿using Shouldly;
+using System.Runtime.Serialization;
+namespace BowlingGame.Tests
+{
+    public class BowlingGameTests
+	{
+        BowlingGame game;
+        public BowlingGameTests()
+        {
+			//Arrange
+			game = new BowlingGame();
+		}
+
+        [Fact]
+        public void RollZeroShouldReturnZero()
+        {
+            //Act
+            game.Roll(0);
+
+			//Assert
+			game.Score().ShouldBe(0);
+		}
+
+
+        [Fact]
+        public void OpenFrameShouldReturnSumOfPins()
+        {
+			//Act
+			game.Roll(3);
+            game.Roll(6);
+
+			//Assert
+			game.Score().ShouldBe(9);
+		}
+
+		[Theory]
+		[InlineData(3, 6, 9)]
+		public void OpenFrameShouldReturnSumOfPins2(int firstBall, int secondBall, int expectedScore)
+		{
+			//Act
+			game.Roll(firstBall);
+			game.Roll(secondBall);
+
+			//Assert
+			game.Score().ShouldBe(expectedScore);
+		}
+	}
+}
