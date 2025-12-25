d82b6c45b79e79cf70791b3aee13e71664fcd9ee
mina24nagy <mina24nagy@gmail.com>
2025-12-25 03:28:29 +0200

    some refactors

--- BowlingGame/BowlingGame.cs
+++ BowlingGame/BowlingGame.cs
@@ -18,26 +18,36 @@
 			int rollIndex = 0;
 
 			for (int frame = 0; frame < 10; frame++)
-			{
-				if (rolls[rollIndex] == 10) // Strike
-				{
-					// Score = 10 + next two balls
-					score += 10 + rolls[rollIndex + 1] + rolls[rollIndex + 2];
-					rollIndex += 1; // Strike only consumes ONE ball slot
-				}
-				else if (rolls[rollIndex] + rolls[rollIndex + 1] == 10) // Spare
-				{
-					// Score = 10 + next one ball
-					score += 10 + rolls[rollIndex + 2];
-					rollIndex += 2; // Spare consumes TWO ball slots
-				}
-				else // Open Frame
-				{
-					score += rolls[rollIndex] + rolls[rollIndex + 1];
-					rollIndex += 2; // Normal frame consumes TWO ball slots
-				}
-			}
-			return score;
+            {
+                if (IsStrike(rollIndex))
+                {
+                    // Score = 10 + next two balls
+                    score += 10 + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+                    rollIndex += 1; // Strike only consumes ONE ball slot
+                }
+                else if (IsSpare(rollIndex))
+                {
+                    // Score = 10 + next one ball
+                    score += 10 + rolls[rollIndex + 2];
+                    rollIndex += 2; // Spare consumes TWO ball slots
+                }
+                else // Open Frame
+                {
+                    score += rolls[rollIndex] + rolls[rollIndex + 1];
+                    rollIndex += 2; // Normal frame consumes TWO ball slots
+                }
+            }
+            return score;
 		}
-	}
+
+        private bool IsSpare(int rollIndex)
+        {
+            return rolls[rollIndex] + rolls[rollIndex + 1] == 10;
+        }
+
+        private bool IsStrike(int rollIndex)
+        {
+            return rolls[rollIndex] == 10;
+        }
+    }
 }
